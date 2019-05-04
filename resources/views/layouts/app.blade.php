b3ba350bedbb66b23441a3e1a64f4c3f1b9854e8
Ali Rasheed <arshaikh_17@hotmail.com>
2019-05-04 21:57:26 +0100

    Renamed admin folder in public.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 	<!-- Scripts -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="{{ asset('js/app.js') }}" defer></script>
-	<script src="{{ asset('admin/js/admin.js') }}" defer></script>
+	<script src="{{ asset('admins/js/admin.js') }}" defer></script>
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 	<!-- Styles -->
