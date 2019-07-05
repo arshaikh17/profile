e826d8a393f5a4406eb426174a623e3052b72fa0
Ali Rasheed <ali@wirebox.co.uk>
2019-07-05 21:01:38 +0100

    Updated comics issues form UI.

--- resources/views/comics/admin/issues/form.blade.php
+++ resources/views/comics/admin/issues/form.blade.php
@@ -9,7 +9,7 @@
 				@if (isset($selectedArc)) under {{ $selectedArc->title }} Arc @endif
 			</h1>
 		</div>
-		<div class="col-6 offset-3">
+		<div class="col-6">
 			@if ($issue->cover)
 				<div class="text-center mb-3">
 					<span>
@@ -26,122 +26,128 @@
 				enctype="multipart/form-data"
 			>
 				{{csrf_field()}}
-				<div class="form-group">
-					<label>Title</label>
-					<input
-						type="text"
-						class="form-control"
-						value="{{ $issue->title }}"
-						name="title"
-						required
-					/>
-				</div>
-				<div class="form-group">
-					<label>Issue Number</label>
-					<input
-						type="text"
-						class="form-control"
-						value="{{ $issue->issue }}"
-						name="issue"
-						required
-					/>
-				</div>
-				<div class="form-group">
-					<label>Cover</label>
-					<input
-						type="file"
-						class="form-control"
-						name="cover"
-						{{ $issue->id ? "" : "required" }}
-					/>
-				</div>
-				<div class="form-group">
-					<label>Owned Status</label>
-					<select
-						class="form-control"
-						name="owned_status"
-						required
-					>
-						<option selected disabled>Set status</option>
-						@forelse ($statuses as $statusKey => $status)
-							<option
-								value="{{ $statusKey }}"
-								@if ($issue->owned_status == $statusKey) selected @endif
+				<div class="row">
+					<div class="col-md-8">
+						<div class="form-group">
+							<label>Title</label>
+							<input
+								type="text"
+								class="form-control"
+								value="{{ $issue->title }}"
+								name="title"
+								required
+							/>
+						</div>
+						<div class="form-group">
+							<label>Issue Number</label>
+							<input
+								type="text"
+								class="form-control"
+								value="{{ $issue->issue }}"
+								name="issue"
+								required
+							/>
+						</div>
+						<div class="form-group">
+							<label>Cover</label>
+							<input
+								type="file"
+								class="form-control"
+								name="cover"
+								{{ $issue->id ? "" : "required" }}
+							/>
+						</div>
+						<div class="form-group">
+							<label>Owned Status</label>
+							<select
+								class="form-control"
+								name="owned_status"
+								required
 							>
-								{{ $status }}
-							</option>
-						@empty
-						@endforelse
-					</select>
-				</div>
-				<h3>Attach Series and Arc</h3>
-				<div class="form-group">
-					<label>Select Series</label>
-					<select
-						class="form-control"
-						name="series_id"
-						required
-					>
-						<option selected>No Series</option>
-						@forelse ($series as $singleSeries)
-							<option
-								value="{{ $singleSeries->id }}"
-								@if (!isset($selectedSeries) && $issue->series_id == $singleSeries->id) selected @endif
-								@if (isset($selectedSeries) && $selectedSeries->id == $singleSeries->id) selected @endif
-							>
-								{{ $singleSeries->title }}
-							</option>
-						@empty
-						@endforelse
-					</select>
-				</div>
-				<div class="form-group">
-					<label>Select Arc</label>
-					<select
-						class="form-control"
-						name="arc_id"
-						required
-					>
-						<option selected>No Arc</option>
-						@forelse ($series as $singleSeries)
-							<optgroup label="{{ $singleSeries->title }}">
-								@forelse ($singleSeries->arcs as $arc)
+								<option selected disabled>Set status</option>
+								@forelse ($statuses as $statusKey => $status)
 									<option
-										value="{{ $arc->id }}"
-										@if (!isset($selectedArc) && $issue->arc_id == $arc->id) selected @endif
-										@if (isset($selectedArc) && $selectedArc->id == $arc->id) selected @endif
+										value="{{ $statusKey }}"
+										@if ($issue->owned_status == $statusKey) selected @endif
 									>
-										{{ $arc->title }}
+										{{ $status }}
 									</option>
 								@empty
-									<option disabled>No arcs in this series</option>
 								@endforelse
-							</optgroup>
-						@empty
-						@endforelse
-					</select>
-				</div>
-				<h3>Attach Authors</h3>
-				<div class="form-group">
-					<label>Authors</label>
-					<select
-						class="form-control"
-						name="author_ids[]"
-						multiple
-						required
-						style="height: 300px;"
-					>
-						<option disabled>Select authors</option>
-						@forelse ($authors as $author)
-							<option
-								value="{{ $author->id }}"
-								@if ($issue->authors && $issue->authors->contains($author)) selected @endif
+							</select>
+						</div>
+						<h3>Attach Series and Arc</h3>
+						<div class="form-group">
+							<label>Select Series</label>
+							<select
+								class="form-control"
+								name="series_id"
+								required
 							>
-								{{ $author->name }}
-							</option>
-						@empty
-						@endforelse
-					</select>
+								<option selected>No Series</option>
+								@forelse ($series as $singleSeries)
+									<option
+										value="{{ $singleSeries->id }}"
+										@if (!isset($selectedSeries) && $issue->series_id == $singleSeries->id) selected @endif
+										@if (isset($selectedSeries) && $selectedSeries->id == $singleSeries->id) selected @endif
+									>
+										{{ $singleSeries->title }}
+									</option>
+								@empty
+								@endforelse
+							</select>
+						</div>
+						<div class="form-group">
+							<label>Select Arc</label>
+							<select
+								class="form-control"
+								name="arc_id"
+								required
+							>
+								<option selected>No Arc</option>
+								@forelse ($series as $singleSeries)
+									<optgroup label="{{ $singleSeries->title }}">
+										@forelse ($singleSeries->arcs as $arc)
+											<option
+												value="{{ $arc->id }}"
+												@if (!isset($selectedArc) && $issue->arc_id == $arc->id) selected @endif
+												@if (isset($selectedArc) && $selectedArc->id == $arc->id) selected @endif
+											>
+												{{ $arc->title }}
+											</option>
+										@empty
+											<option disabled>No arcs in this series</option>
+										@endforelse
+									</optgroup>
+								@empty
+								@endforelse
+							</select>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<h3>Attach Authors</h3>
+							<div class="form-group">
+								<label>Authors</label>
+								<select
+									class="form-control"
+									name="author_ids[]"
+									multiple
+									required
+									style="height: 900px;"
+								>
+									<option disabled>Select authors</option>
+									@forelse ($authors as $author)
+										<option
+											value="{{ $author->id }}"
+											@if ($issue->authors && $issue->authors->contains($author)) selected @endif
+										>
+											{{ $author->name }}
+										</option>
+									@empty
+									@endforelse
+								</select>
+							</div>
+					</div>
 				</div>
 				<div class="form-group">
 					<input
