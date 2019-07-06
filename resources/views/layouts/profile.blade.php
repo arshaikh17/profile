c7b40bd82fbecf5f81e87177fca26fdcc38ad71a
Ali Rasheed <ali@wirebox.co.uk>
2019-07-06 21:36:32 +0100

    Fixed title in profile layout.

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ali Rasheed - @yield("title")</title>
+	<title>Ali Rasheed @yield("title")</title>
 	
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
