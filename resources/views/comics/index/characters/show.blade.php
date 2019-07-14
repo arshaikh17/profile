0d74e3f6b167bc66e98acc90c8337768e598f255
Ali Rasheed <ali@wirebox.co.uk>
2019-07-14 15:12:53 +0100

    Fixed coding standard.

--- resources/views/comics/index/characters/show.blade.php
+++ resources/views/comics/index/characters/show.blade.php
@@ -71,68 +71,67 @@
 			<div class="">
 				@forelse ($series as $seriesKey => $singleSeries)
 				
-				@php
-					$singleIssues		 =	$singleSeries->singleIssues;
-				@endphp
-				
-				<div
-					class="container series-container masonry-grid"
-					data-toggle-dom-value="series_container_{{ $singleSeries->id }}"
-					data-masonry-parent=".arcs-row"
-					data-masonry-child=".arc"
-				>
+					@php
+						$singleIssues	 =	$singleSeries->singleIssues;
+					@endphp
 					
-					<h3 class="bg-dark shadow p-3 mt-2 text-white">{{ $singleSeries->title }}</h3>
-					
-					<div class="row">
-						@if ($singleSeries->arcs->count())
-							<div class="col-12 col-md-8">
-								<h4>Arcs</h4>
-								<div
-									class="row arcs-row"
-								>
-									@forelse ($singleSeries->arcs as $arc)
-									<div class="col-12 col-md-6 mb-4 arc">
-										<div class="shadow p-4 bg-dark text-white">
-											{{ $arc->title }}
-											<ul>
-												@forelse ($arc->issues as $issue)
-												<li>#{{ $issue->issue . ' - ' . $issue->title }}</li>
-												@empty
-												@endforelse
-											</ul>
+					<div
+						class="container series-container masonry-grid"
+						data-toggle-dom-value="series_container_{{ $singleSeries->id }}"
+						data-masonry-parent=".arcs-row"
+						data-masonry-child=".arc"
+					>
+						
+						<h3 class="bg-dark shadow p-3 mt-2 text-white">{{ $singleSeries->title }}</h3>
+						
+						<div class="row">
+							@if ($singleSeries->arcs->count())
+								<div class="col-12 col-md-8">
+									<h4>Arcs</h4>
+									<div
+										class="row arcs-row"
+									>
+										@forelse ($singleSeries->arcs as $arc)
+										<div class="col-12 col-md-6 mb-4 arc">
+											<div class="shadow p-4 bg-dark text-white">
+												{{ $arc->title }}
+												<ul>
+													@forelse ($arc->issues as $issue)
+													<li>#{{ $issue->issue . ' - ' . $issue->title }}</li>
+													@empty
+													@endforelse
+												</ul>
+											</div>
 										</div>
+										@empty
+										<div class="col-12"><p>No arcs under {{ $singleSeries->title }}</p></div>
+										@endforelse
 									</div>
+								</div>
+								<div class="col-12 col-md-4">
+									<h4>Single Issues</h4>
+									@forelse ($singleIssues as $singleIssue)
+										<div class="border border-secondary shadow p-2">#{{ $singleIssue->issue . " - " . $singleIssue->title }}</div>
 									@empty
-									<div class="col-12"><p>No arcs under {{ $singleSeries->title }}</p></div>
+										<p>No single issues.</p>
 									@endforelse
 								</div>
-							</div>
-							<div class="col-12 col-md-4">
-								<h4>Single Issues</h4>
+							@else
 								@forelse ($singleIssues as $singleIssue)
-									<div class="border border-secondary shadow p-2">#{{ $singleIssue->issue . " - " . $singleIssue->title }}</div>
+									<div class="col-12 col-md-4 mb-2">
+										<div class="bg-dark text-white shadow p-2">#{{ $singleIssue->issue . " - " . $singleIssue->title }}</div>
+									</div>
 								@empty
 									<p>No single issues.</p>
 								@endforelse
-							</div>
-						@else
-							@forelse ($singleIssues as $singleIssue)
-								<div class="col-12 col-md-4 mb-2">
-									<div class="bg-dark text-white shadow p-2">#{{ $singleIssue->issue . " - " . $singleIssue->title }}</div>
-								</div>
-							@empty
-								<p>No single issues.</p>
-							@endforelse
-						@endif
+							@endif
+						</div>
 					</div>
-				</div>
 				@empty
-				<p>No comics under {{ $character->name }}</p>
+					<p>No comics under {{ $character->name }}</p>
 				@endforelse
 			</div>
 		</div>
-		
 	</div>
 </div>
 @endsection
