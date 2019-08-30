b28af8bbc9d10bd5bb61ced93213937559d057e1
Ali Rasheed <ali@wirebox.co.uk>
2019-08-30 19:57:01 +0100

    Added expenses nav button.

--- resources/views/partials/admin-nav.blade.php
+++ resources/views/partials/admin-nav.blade.php
@@ -9,6 +9,9 @@
 		<li class="nav-item ml-2">
 			<a class="nav-link btn btn-sm btn-light" href="{{ route('wishlists.index') }}">Wishlists</a>
 		</li>
+		<li class="nav-item ml-2">
+			<a class="nav-link btn btn-sm btn-info text-white" href="{{ route('expenses.index') }}">Expenses</a>
+		</li>
 	</ul>
 	<ul class="navbar-nav">
 		<li class="nav-item">
